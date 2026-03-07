795cc2b663d4e4584dfdbef2ec99024bda2c3968
Rodion2901 <rodioncernyh6@gmail.com>
2026-03-07 21:26:52 +0300

    чекпоинты добавил

--- train_PPO.py
+++ train_PPO.py
@@ -1,10 +1,12 @@
 from Game import game_2048
-from Game import pretty_print
 from PPO import PPOAgent
 from PPO import Memory
-import numpy as np
-episodes = 1000
+from PPO import checkpoint
+from PPO import load_checkpoint
+from Game import pretty_print
 
+episodes = 20000
+start_episode = 10000
 def source_max_result(map):
     a = 0
     for i in map:
@@ -21,11 +23,15 @@ reward_history = []
 update_timestep = 2000
 time_step = 0
 max_results = []
-for episode in range(1, episodes+1):
+if start_episode != 1:
+    agent = load_checkpoint(agent, start_episode)
+for episode in range(start_episode, episodes+1):
     state = game.reset()
     done = False
     total_reward = 0
     while not done:
+        if episode % 500 == 0:
+             pretty_print(state)
         time_step += 1
         action = agent.act(state, memory)
         next_state, reward, done = game.step(action)
@@ -38,6 +44,8 @@ for episode in range(1, episodes+1):
         state = next_state
         
         total_reward += reward
+        if episode % 500 == 0:
+             pretty_print(state)
     reward_history.append(total_reward)
     max_results.append(source_max_result(state))
     if episode % 100 == 0:
@@ -45,3 +53,6 @@ for episode in range(1, episodes+1):
                 f"Episode: {episode}, "
                 f"максимальный результат: {max(max_results)}"
             )
+    if episode % 500 == 0:
+        checkpoint(agent.policy, agent.optimizer, episode)
+print(max(max_results))
